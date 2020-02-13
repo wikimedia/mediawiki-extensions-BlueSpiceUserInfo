3918e18479237c3a43649f91b98a77eb46620c6a
rvogel <vogel@hallowelt.biz>
2020-02-13 16:55:30 +0100

    Load resources conditionally
    ERM18233
    [3.1.x]
    
    Change-Id: I2fd90130c0f3b2ecd90c659d8649a5672531b725

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -7,7 +7,6 @@ use BlueSpice\Hook\BeforePageDisplay;
 class AddResources extends BeforePageDisplay {
 
 	protected function doProcess() {
-		$this->out->addModuleStyles( 'ext.bluespice.userinfo.styles' );
 		$this->out->addModules( 'ext.bluespice.userinfo' );
 		return true;
 	}
